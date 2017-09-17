6b910f3e822f8004e473c6bfca314c7b7f34e4ac
John McDonald <palisades.lakes@gmail.com>
2017-09-17 15:34:07 -0700

    pom consistency

--- src/scripts/clojure/palisades/lakes/funx/l2norm/primitive.clj
+++ src/scripts/clojure/palisades/lakes/funx/l2norm/primitive.clj
@@ -1,25 +1,19 @@
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* :warn-on-boxed)
 ;;----------------------------------------------------------------
-(ns palisades.lakes.funx.l2norm.bench
+(ns palisades.lakes.funx.l2norm.primitive
   "Use criterium for alternative function implementations."
   {:author "palisades dot lakes at gmail dot com"
    :since "2017-09-16"
-   :version "2017-09-17"}
+   :version "2017-09-16"}
   (:require [palisades.lakes.bench.prng :as prng]
             [palisades.lakes.bench.core :as bench]
             [palisades.lakes.funx.l2norm.defs :as defs]))
 ;;----------------------------------------------------------------
-(def options {:n (* 4 4 1024 1024) :samples 200})
-(bench/bench 
+(def options {:n (* 64 1024 1024) :samples 1024})
+(bench/profile
   [prng/doubles defs/udouble]
-  [defs/inline
-   defs/invokestatic
-   defs/primitive
-   defs/boxprimitive
-   defs/cljmeta
-   defs/funxmeta
-   defs/boxed]
+  [defs/primitive]
   options)
 ;;----------------------------------------------------------------
 #_(shutdown-agents)
